0734b335e7ed785a9f5c244ef2e8a4b44de05293
kekich <105925379+kekich119@users.noreply.github.com>
2022-08-14 19:34:03 +0300

    я написал лютый тест на C#

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = button2.Location.X;
+            int y = button2.Location.Y;
+            
+            if (x < 10)
+            {
+                button2.Location = new System.Drawing.Point(100, y);
+            }
+            else
+            {
+                button2.Location = new System.Drawing.Point(x - 10, y);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("Я ТАК И ЗНАЛ!!!");
+        }
+    }
+    }
